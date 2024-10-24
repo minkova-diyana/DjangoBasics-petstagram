2744aeb9dfe754653067c48e4adf0a222ddc0df3
minkova-diyana <minkova.di@gmail.com>
2024-10-24 16:35:29 +0300

    \first commit

---
+++ petstagram/common/views.py
@@ -0,0 +1,9 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+# Create your views here.
+
+
+def home_page(request):
+    return render(request, 'common/home-page.html')
---
+++ petstagram/pets/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+
+class Pet(models.Model):
+    name = models.CharField(max_length=30)
+    personal_photo = models.URLField()
+    date_of_birth = models.DateField(blank=True, null=True)
+    slug = models.SlugField(unique=True, null=True, blank=True)
