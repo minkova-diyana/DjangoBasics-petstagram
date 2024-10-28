a4a538fb1956b211f79bea6545488a3dae897a10
petstagram <git@github.com:minkova-diyana/petstagram.git>
2024-10-28 12:15:35 +0200

    develope the models ad admin files of the project

--- petstagram/photos/models.py
+++ petstagram/photos/models.py
@@ -2,11 +2,14 @@ from django.core.validators import MinLengthValidator
 from django.db import models
 
 from petstagram.pets.models import Pet
+from petstagram.photos.validators import SizeImgValidator
 
 
 # Create your models here.
 class Photo(models.Model):
-    photo = models.ImageField()
+    photo = models.ImageField(validators=[
+        SizeImgValidator,
+    ])
     description = models.TextField(
         max_length=300,
         validators=[
@@ -15,6 +18,7 @@ class Photo(models.Model):
         blank=True,
         null=True
     )
+    location = models.CharField(max_length=30, null=True, blank=True)
     tagged_pets = models.ManyToManyField(Pet, blank=True)
     date_of_publication = models.DateField(
         auto_now=True
